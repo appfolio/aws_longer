50d77c86d4b422604c61217da59a2a50244a4ef9
Bryce Boe <bbzbryce@gmail.com>
2021-04-28 16:33:43 -0700

    Bump to version 0.4.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## Unreleased
+## 0.4.0 (2021/04/28)
 
 __Changed__
 
--- aws_longer/__init__.py
+++ aws_longer/__init__.py
@@ -12,7 +12,7 @@ import botocore
 import keyring
 
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 ACCOUNT_MAPPING_FILENAME = os.path.expanduser("~/.aws/accounts")
 KEYRING_SERVICE_NAME = "aws_longer"
 ROLE_TOKEN_DURATION = 3600
